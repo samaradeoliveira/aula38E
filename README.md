41923692db90b68f1db9f45251ea2629cb36d58a
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-04-04 15:21:01 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aula38E
+# C36RV_SpeedRacer_StudentActivity
+Student Activity
---
+++ sketch.js
@@ -0,0 +1,51 @@
+//variáveis 
+var canvas;
+var backgroundImage;
+var database;
+var form, player;
+var playerCount;
+var game;
+//declaração de vars
+var car1, car2, car1_img, car2_img, cars, track;
+var gameState;
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  //carregar as imagens em: car1_img, car2_img, track
+
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  //Iniciando banco de dados
+  database = firebase.database();
+
+  //objeto de jogo é criado
+  game = new Game();
+  //chamando um método da classe game
+  game.start();
+
+}
+
+function draw() {
+  background(backgroundImage);
+
+   //os if's
+   /*if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }*/
+
+
+ 
+}
+
+//sempre que há atualização ela ajusta automaticamente o tamanho da tela. 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
